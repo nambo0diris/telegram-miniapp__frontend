2daaa24a267fd37f718b31cf786726504b2e1b0d
Anzor Khaindrava <a.haindrava@parisnail.ru>
2024-05-28 12:13:03 +0300

    added redux

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
+import {useAppDispatch} from "../../hooks/redux-hooks.ts";
+
 
 function App() {
+    const dispatch = useAppDispatch();
 
-  return (
-    <div className={"p-[20px]"}>
-      dasd
-    </div>
-  )
+    return (
+        <div className={"p-[20px]"}>
+
+        </div>
+    )
 }
 
 export default App
---
+++ src/hooks/redux-hooks.ts
@@ -0,0 +1,10 @@
+
+import {AppDispatch, AppStore, RootState} from "../services/store/store.ts";
+import {
+    useDispatch as dispatchHook,
+    useSelector as selectorHook, useStore,
+} from 'react-redux'
+import type {TypedUseSelectorHook} from "react-redux"
+export const useAppDispatch: () => AppDispatch = dispatchHook
+export const useAppSelector: TypedUseSelectorHook<RootState> = selectorHook
+export const useAppStore: () => AppStore = useStore
